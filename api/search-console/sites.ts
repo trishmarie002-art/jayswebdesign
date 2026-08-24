296282e411982c2f5e100535c8c6ddf3f78644a6
trishmarie002-art <trishmarie002@gmail.com>
2026-08-23 23:07:36 -0500

    Add Search Console property endpoint

---
+++ api/search-console/sites.ts
@@ -0,0 +1,18 @@
+import { getAccessToken } from "../../lib/searchConsole";
+
+export default async function handler(req: any, res: any) {
+  const token = await getAccessToken(req, res);
+  if (!token) return res.status(401).json({ error: "Search Console is not connected." });
+
+  const response = await fetch("https://www.googleapis.com/webmasters/v3/sites", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const data: any = await response.json();
+  if (!response.ok) return res.status(response.status).json({ error: data?.error?.message || "Unable to load Search Console properties." });
+
+  const sites = (data.siteEntry || []).map((site: any) => ({
+    siteUrl: site.siteUrl,
+    permissionLevel: site.permissionLevel,
+  }));
+  res.status(200).json({ sites });
+}
