547a34f3498ef759e85f2f7698bca9178c369f4a
trishmarie002-art <trishmarie002@gmail.com>
2026-08-28 14:21:13 -0500

    Remove unused rank checker file api/search-console/disconnect.ts

--- api/search-console/disconnect.ts
+++
@@ -1,6 +0,0 @@
-import { clearSessionCookie } from "../../lib/searchConsole.js";
-
-export default function handler(req: any, res: any) {
-  clearSessionCookie(res);
-  res.redirect("/google-rank-tracker");
-}
